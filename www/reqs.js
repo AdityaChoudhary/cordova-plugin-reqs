9d6c6072f6af798a9231343e714182e40a22956a
AdityaChoudhary <iamadityaaa@gmail.com>
2017-04-02 13:20:00 +0530

    Update 6.0

--- www/reqs.js
+++ www/reqs.js
@@ -1,29 +1,27 @@
-cordova.define('cordova-plugin-reqs.ReqsPlugin', function (require, exports, module) {
-	(function () {
-		var exec = cordova.require('cordova/exec');
+(function () {
+	var exec = cordova.require('cordova/exec');
 
-		module.exports = function (data) {
-			return new Promise(function (done, fail) {
-				exec(function (resp) {
+	module.exports = function (data) {
+		return new Promise(function (done, fail) {
+			exec(function (resp) {
 
-					if (resp.status < 100 || resp.status > 599) {
-						fail(new TypeError('Network request failed'));
-						return
+				if (resp.status < 100 || resp.status > 599) {
+					fail(new TypeError('Network request failed'));
+					return;
+				}
+
+				done({
+					url: resp.url,
+					status: resp.status,
+					responseText: resp.responseText,
+					getAllResponseHeaders: function () {
+						return resp.responseHeaders;
 					}
+				});
+			}, function (resp) {
+				fail(new TypeError('Network request failed'))
 
-					done({
-						url: resp.url,
-						status: resp.status,
-						responseText: resp.responseText,
-						getAllResponseHeaders: function () {
-							return resp.responseHeaders;
-						}
-					});
-				}, function (resp) {
-					fail(new TypeError('Network request failed'))
-
-				}, 'ReqsPlugin', 'gets', [data.method, data.url, null, data.headers]);
-			})
-		};
-	})();
-});
+			}, 'ReqsPlugin', 'gets', [data.method, data.url, null, data.headers]);
+		})
+	};
+})();
